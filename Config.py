d7e4525bec5f3d25f2a73c282764259060c23edf
SaikotHouse <65276816+saikothouse@users.noreply.github.com>
2024-05-04 23:57:15 +0600

    Add files via upload

---
+++ Config.py
@@ -0,0 +1,11 @@
+# Fillout The variables in Config.py further queries @riz4d 0n telegram
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+API_ID = int(os.environ.get("API_ID"))
+API_HASH = os.environ.get("API_HASH")
+
+# If You're hosting in VPS replace the following lines 9,10,11 hash symbol and replacing the approriate values on it and put the hash symbol before the lines 3,4,5
+
+BOT_TOKEN = "6887423345:AAGGYXrylfsg5y1lKQI3WyGyt-6SymIqUo8"
+API_ID = "15452337"
+API_HASH = "969f9da96a2b16928f59a04025b14311"
---
+++ main.py
@@ -0,0 +1,96 @@
+# copyright 2020-22 @Mohamed Rizad
+# Telegram @riz4d
+# Instagram @riz.4d
+from pyrogram import *
+import requests as re
+from Config import *
+from pyrogram.types import InlineKeyboardButton,InlineKeyboardMarkup
+import wget
+import os 
+
+buttons=InlineKeyboardMarkup(
+                             [
+                             [
+            InlineKeyboardButton('Generate', callback_data='generate'),
+            InlineKeyboardButton('Refresh', callback_data='refresh'),
+            InlineKeyboardButton('Close', callback_data='close')
+                   ] 
+                             ])
+
+msg_buttons=InlineKeyboardMarkup(
+                             [
+                             [
+            InlineKeyboardButton('View message', callback_data='view_msg'),
+            InlineKeyboardButton('Close', callback_data='close')
+                   ] 
+                             ])
+
+
+app=Client('Temp-Mail Bot',
+           api_id=API_ID,
+           api_hash=API_HASH,
+           bot_token=BOT_TOKEN)
+
+email=''
+@app.on_message(filters.command('start'))
+async def start_msg(client,message):
+    await message.reply("**Hey "+message.from_user.first_name+" !!**\n @TempSecMailBot is a free service that allows to generates and receive emails at a temporary address that self-destructed after a certain time elapses.\n\n**__ How It Safe's You??**__\n- Using the temporary mail allows you to completely protect your real mailbox against the loss of personal information. Your temporary e-mail address is completely anonymous. Your details: information about your person and users with whom you communicate, IP-address, e-mail address are protected and completely confidential.\n\nFurther Queris @noob_xd🌚")
+    await message.reply("**Generate a Email Now❕**",
+                        reply_markup=buttons)
+@app.on_callback_query()
+async def mailbox(client,message):
+    response=message.data
+    if response=='generate':
+       global email
+       email = re.get("https://www.1secmail.com/api/v1/?action=genRandomMailbox&count=1").json()[0]
+       await message.edit_message_text('__**Your Temporary E-mail: **__`'+str(email)+'`',
+                                       reply_markup=buttons)
+       print(email)
+    elif response=='refresh':
+        print(email)
+        try:
+            if email=='':
+                await message.edit_message_text('Genaerate a email',reply_markup=buttons)
+            else: 
+                getmsg_endp =  "https://www.1secmail.com/api/v1/?action=getMessages&login=" + email[:email.find("@")] + "&domain=" + email[email.find("@") + 1:]
+                print(getmsg_endp)
+                ref_response = re.get(getmsg_endp).json()
+                global idnum
+                idnum=str(ref_response[0]['id'])
+                from_msg=ref_response[0]['from']
+                subject=ref_response[0]['subject']
+                refreshrply='You a message from '+from_msg+'\n\nSubject : '+subject
+                await message.edit_message_text(refreshrply,
+                                                reply_markup=msg_buttons)
+        except:
+            await message.answer('No messages were received..\nin your Mailbox '+email)
+    elif response=='view_msg':
+        msg =re.get("https://www.1secmail.com/api/v1/?action=readMessage&login=" + email[:email.find("@")] + "&domain=" + email[email.find("@") + 1:] + "&id=" + idnum).json()
+        print(msg)
+        from_mail=msg['from']
+        date=msg['date']
+        subjectt=msg['subject']
+        try:
+          attachments=msg['attachments'][0]
+        except:
+            pass
+        body=msg['body']
+        mailbox_view='ID No : '+idnum+'\nFrom : '+from_mail+'\nDate : '+date+'\nSubject : '+subjectt+'\nmessage : \n'+body
+        await message.edit_message_text(mailbox_view,reply_markup=buttons)
+        mailbox_view='ID No : '+idnum+'\nFrom : '+from_mail+'\nDate : '+date+'\nSubject : '+subjectt+'\nmessage : \n'+body
+        if attachments == "[]":
+            await message.edit_message_text(mailbox_view,reply_markup=buttons)
+            await message.answer("No Messages Were Recieved..", show_alert=True)
+        else:
+            dlattach=attachments['filename']
+            attc="https://www.1secmail.com/api/v1/?action=download&login=" + email[:email.find("@")] + "&domain=" + email[email.find("@") + 1:] + "&id=" + idnum+"&file="+dlattach
+            print(attc)
+            mailbox_vieww='ID No : '+idnum+'\nFrom : '+from_mail+'\nDate : '+date+'\nSubject : '+subjectt+'\nmessage : \n'+body+'\n\n'+'[Download]('+attc+') Attachments'
+            filedl=wget.download(attc)
+            await message.edit_message_text(mailbox_vieww,reply_markup=buttons)
+            os.remove(dlattach)
+    elif response=='close':
+        await message.edit_message_text('Session Closed✅')
+app.run()
+
+# Stay tuned for more : Instagram[@riz.4d]
